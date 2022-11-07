7d9ca0d48407678f8ae20ed29cc76675204aaa8a
barnawalabhinav <abhinav.barnawal.iitd@gmail.com>
2022-11-07 21:25:20 +0530

    Added particle filtering

--- estimator.py
+++ estimator.py
@@ -1,4 +1,6 @@
+import random
 import util
+import math
 from util import Belief, pdf
 from engine.const import Const
 
@@ -11,17 +13,17 @@ class Estimator(object):
     def __init__(self, numRows: int, numCols: int):
         self.belief = util.Belief(numRows, numCols)
         self.transProb = util.loadTransProb()
+        self.particles = [i*numCols +
+                          j for j in range(numCols) for i in range(numRows)]
 
-        for r in range(numRows):
-            for c in range(numCols):
-                self.belief.setProb(r, c, 0.0)
+        # print(self.transProb)
 
     ##################################################################################
     # [ Estimation Problem ]
     # Function: estimate (update the belief about a StdCar based on its observedDist)
     # ----------------------
     # Takes |self.belief| -- an object of class Belief, defined in util.py --
-    # and updates it *inplace* based onthe distance observation and your current position.
+    # and updates it *inplace* based on the distance observation and your current position.
     #
     # - posX: x location of AutoCar
     # - posY: y location of AutoCar
@@ -46,9 +48,23 @@ class Estimator(object):
 
         rows = self.belief.numRows
         cols = self.belief.numCols
+        weights = [0 for k in range(cols*rows)]
+        for particle in self.particles:
+            y = util.rowToY(particle // cols)
+            x = util.colToX(particle % cols)
+            estimated_dist = math.sqrt(
+                (x-posX) * (x-posX) + (y-posY) * (y-posY))
+            weights[particle] = 1 / abs(estimated_dist - observedDist)
+
         for r in range(rows):
             for c in range(cols):
-                self.belief.addProb(r, c, 0.001)
+                self.belief.setProb(r, c, 0)
+
+        self.particles = random.choices(self.particles, weights, k=rows*cols)
+        for particle in self.particles:
+            r = particle // cols
+            c = particle % cols
+            self.belief.addProb(r, c, 1)
         self.belief.normalize()
 
         # END_YOUR_CODE
