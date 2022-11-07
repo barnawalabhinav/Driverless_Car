1c423081b8a94ddb87fab9562e404f873a404bd0
barnawalabhinav <abhinav.barnawal.iitd@gmail.com>
2022-11-07 23:33:37 +0530

    implemented basic particle filter

--- estimator.py
+++ estimator.py
@@ -13,10 +13,7 @@ class Estimator(object):
     def __init__(self, numRows: int, numCols: int):
         self.belief = util.Belief(numRows, numCols)
         self.transProb = util.loadTransProb()
-        self.particles = [i*numCols +
-                          j for j in range(numCols) for i in range(numRows)]
-
-        # print(self.transProb)
+        self.particles = [i*numCols + j for j in range(numCols) for i in range(numRows) for k in range(2)]
 
     ##################################################################################
     # [ Estimation Problem ]
@@ -49,25 +46,7 @@ class Estimator(object):
         rows = self.belief.numRows
         cols = self.belief.numCols
 
-        for i in range(len(self.particles)):
-            particle = self.particles[i]
-            y = particle // cols
-            x = particle % cols
-            moving_prob = [self.transProb[((x, y), (x, y))] if ((x, y), (x, y)) in self.transProb else 0,
-                        self.transProb[((x, y), (x+1, y))] if ((x, y), (x+1, y)) in self.transProb else 0,
-                        self.transProb[((x, y), (x-1, y))] if ((x, y), (x-1, y)) in self.transProb else 0,
-                        self.transProb[((x, y), (x, y+1))] if ((x, y), (x, y+1)) in self.transProb else 0,
-                        self.transProb[((x, y), (x, y-1))] if ((x, y), (x, y-1)) in self.transProb else 0,
-                    self.transProb[((x, y), (x+1, y+1))] if ((x, y), (x+1, y+1)) in self.transProb else 0,
-                    self.transProb[((x, y), (x-1, y+1))] if ((x, y), (x-1, y+1)) in self.transProb else 0,
-                    self.transProb[((x, y), (x+1, y-1))] if ((x, y), (x+1, y-1)) in self.transProb else 0,
-                    self.transProb[((x, y), (x-1, y-1))] if ((x, y), (x-1, y-1)) in self.transProb else 0]
-            total = sum(moving_prob)
-            moving_prob = [prob/total for prob in moving_prob]
-            self.particles[i] = random.choice(
-                [(x, y), (x+1, y), (x-1, y), (x, y+1), (x, y-1)], p=moving_prob, k=1)
-
-        weights = [0 for k in range(cols*rows)]
+        weights = [0 for k in range(len(self.particles))]
         for particle in self.particles:
             y = util.rowToY(particle // cols)
             x = util.colToX(particle % cols)
@@ -79,12 +58,40 @@ class Estimator(object):
             for c in range(cols):
                 self.belief.setProb(r, c, 0)
 
-        self.particles = random.choices(self.particles, weights, k=rows*cols)
+        self.particles = random.choices(self.particles, weights, k=len(self.particles))
         for particle in self.particles:
             r = particle // cols
             c = particle % cols
             self.belief.addProb(r, c, 1)
         self.belief.normalize()
+
+        if not isParked:
+            for i in range(len(self.particles)):
+                particle = self.particles[i]
+                y = particle // cols
+                x = particle % cols
+                moving_prob = [ self.transProb[((y, x), (y-1, x-1))] if ((y, x), (y-1, x-1)) in self.transProb else 0,
+                                self.transProb[((y, x), (y, x-1))] if ((y, x), (y, x-1)) in self.transProb else 0,
+                                self.transProb[((y, x), (y+1, x-1))] if ((y, x), (y+1, x-1)) in self.transProb else 0,
+                                self.transProb[((y, x), (y-1, x))] if ((y, x), (y-1, x)) in self.transProb else 0,
+                                self.transProb[((y, x), (y, x))] if ((y, x), (y, x)) in self.transProb else 0,
+                                self.transProb[((y, x), (y+1, x))] if ((y, x), (y+1, x)) in self.transProb else 0,
+                                self.transProb[((y, x), (y-1, x+1))] if ((y, x), (y-1, x+1)) in self.transProb else 0,
+                                self.transProb[((y, x), (y, x+1))] if ((y, x), (y, x+1)) in self.transProb else 0,
+                                self.transProb[((y, x), (y+1, x+1))] if ((y, x), (y+1, x+1)) in self.transProb else 0]
+                total = sum(moving_prob)
+                if total == 0:
+                    # for i in [-1, 0, 1]:
+                    #     for j in [-1, 0, 1]:
+                    #         if x+i >= 0 and x+i < cols and y+j >= 0 and y+j < rows:
+                    #             moving_prob[(i+1)*3+(j+1)] = 1
+                    #             total += 1
+                    moving_prob[4] = 1
+                else:
+                    moving_prob = [prob/total for prob in moving_prob]
+                self.particles[i] = random.choices(
+                    [x-1+(y-1)*cols, x-1+y*cols, x-1+(y+1)*cols, x+(y-1)*cols, x+y*cols, x+(y+1)*cols, x+1+(y-1)*cols, x+1+y*cols, x+1+(y+1)*cols], moving_prob, k=1)[0]
+                    # [y-1+(x-1)*cols, y+(x-1)*cols, y+1+(x-1)*cols, y-1+x*cols, y+x*cols, y+1+x*cols, y-1+(x+1)*cols, y+(x+1)*cols, y+1+(x+1)*cols], moving_prob, k=1)[0]
 
         # END_YOUR_CODE
         return
